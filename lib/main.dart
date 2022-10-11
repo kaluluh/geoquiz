88a9d759114a3a022cd867c4e1b88c8c37fb0785
Michal <485683@mail.muni.cz>
2022-10-11 09:01:16 +0200

    fix: Firebase default options class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
